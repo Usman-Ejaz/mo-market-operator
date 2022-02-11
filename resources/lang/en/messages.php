df6d3ae9e5bfc1acc484701aea9b1fe4a5233f5f
usman-ejaz <usman.ejaz@nxb.com.pk>
2022-02-11 11:56:43 +0500

    minor change for staging branch

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -4,5 +4,5 @@ return [
     "not_numeric" => "This field cannot be numeric",
     "valid_file_extension" => "Please Attach a file with valid extension",
     "valid_date" => ":first date must be greater than :second date",
-    "ckeditor_required" => "This field is required."
+    "ckeditor_required" => "This field is required.",
 ];
