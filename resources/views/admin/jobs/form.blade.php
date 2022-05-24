4ae49819642c1cb593fb0b894152cc2b61b35f5c
Usman Ejaz <usman.ejaz@nxb.com.pk>
2022-05-24 17:52:16 +0500

    fixes

--- resources/views/admin/jobs/form.blade.php
+++ resources/views/admin/jobs/form.blade.php
@@ -110,9 +110,9 @@
     <div class="row">
         <div class="col-md-12">
             <div class="form-group">
-                <label for="attachments[]">Job Attechments <span class="text-danger">*</span> <small>(Max allowed size is 5MB. Allowed types are doc, docx, pdf)</small> </label>
+                <label for="attachments[]">Job Attachments <span class="text-danger">*</span> <small>(Max allowed size is 5MB. Allowed types are doc, docx, pdf)</small> </label>
                 <input type="file" class="form-control" id="attachments[]" name="attachments[]" multiple>
-                <span class="form-text text-danger">{{ $errors->first('attachments') }} </span>                
+                <span class="form-text text-danger">{{ $errors->first('attachments[]') }} </span>
                 @if(isset($job->attachment_links) && count($job->attachment_links) > 0)
                     @foreach ($job->attachment_links as $file)
                     <small class="text-primary fileExists" style="display: block; margin-bottom: 15px;">
