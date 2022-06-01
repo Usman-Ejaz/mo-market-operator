ad011577dd26c7a8493de5ce11743b2547df6a70
Usman Ejaz <usman.ejaz@nxb.com.pk>
2022-06-01 14:17:17 +0500

    fixes for datetime

--- resources/views/admin/trainings/create.blade.php
+++ resources/views/admin/trainings/create.blade.php
@@ -94,7 +94,6 @@
 			step: 5,
 			roundTime: 'ceil',
 			minDate: new Date(),
-			minTime: new Date(),
 			validateOnBlur: false,
 			onChangeDateTime: function(selectedDateTime, $input) {				
 				
