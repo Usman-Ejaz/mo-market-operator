ff62b3e2095f03e5e7180c868193e3ffbc6a5f99
Rida Arshad <mailto:rida.arshad@nxb.com.pk>
2022-10-06 09:48:25 +0500

    page scroll up on click pagination

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -116,6 +116,12 @@
 			]
 		});
 
+		table.on('page.dt', function () {
+			$('html, body').animate({
+			scrollTop: $(".dataTables_wrapper").offset().top
+				}, 'fast');
+		});
+		
         $('body').on('click', '.deleteButton', (e) => {
             action = e.target.dataset.action;
             $('#deleteModal').modal('toggle');
